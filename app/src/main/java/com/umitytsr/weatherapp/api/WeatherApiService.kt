fa8736baf0e7f501d4824cd818febfcab9573b5f
Ümit Yetişir <umitytsr@gmail.com>
2023-04-27 02:02:38 +0300

    WeatherApiService düzeltmesi

--- app/src/main/java/com/umitytsr/weatherapp/api/WeatherApiService.kt
+++ app/src/main/java/com/umitytsr/weatherapp/api/WeatherApiService.kt
@@ -11,11 +11,11 @@ import retrofit2.http.Query
 interface WeatherApiService {
     @GET("v1/forecast")
     fun getProperties(
-        @Query("latitude") latitude: Double = 40.20,
-        @Query("longitude") longitude: Double = 29.06,
+        @Query("latitude") latitude: Double = 40.7750,
+        @Query("longitude") longitude: Double = 29.9480,
         @Query("current_weather") current_weather: Boolean = true,
-        @Query("daily") daily: String ="weathercode,temperature_2m_max,temperature_2m_min",
-        @Query("timezone") timeZone: String = "Europe%2FBerlin",
+        @Query("timezone") timezone: String = "auto",
+        @Query("daily") daily: String = "weathercode,temperature_2m_max,temperature_2m_min",
         @Query("temperature_unit") temperatureUnit: String = "celsius",
         @Query("forecast_days") forecastDays: Int = 14
 
